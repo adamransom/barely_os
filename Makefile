53881833785bb6f086e5d9b3a5650f21ebb7ae96
Adam Ransom <v0id.gmr@gmail.com>
2016-11-03 12:52:05 +0900

    Create an extremely simple function to turn on LED

--- Makefile
+++ Makefile
@@ -19,6 +19,8 @@ LIST = kernel.list
 # Name of the linker script to use
 LINK_SCRIPT = kernel.ld
 
+OBJECTS := $(patsubst $(SOURCE)%.s,$(BUILD)%.o,$(wildcard $(SOURCE)*.s))
+
 # Rule to build everything (creates the target image and listing)
 all: $(TARGET) $(LIST)
 
@@ -27,11 +29,11 @@ $(TARGET): $(BUILD)kernel.elf
 	$(ARMGNU)-objcopy $(BUILD)kernel.elf -O binary $(TARGET) 
 
 # Rule to make the ELF file
-$(BUILD)kernel.elf: $(BUILD)kernel.o $(LINK_SCRIPT)
-	$(ARMGNU)-ld $< -o $(BUILD)kernel.elf -T $(LINK_SCRIPT)
+$(BUILD)kernel.elf: $(OBJECTS) $(LINK_SCRIPT)
+	$(ARMGNU)-ld $(OBJECTS) -o $(BUILD)kernel.elf -T $(LINK_SCRIPT)
 
 # Rule to make the object files
-$(BUILD)kernel.o: $(SOURCE)kernel.s $(BUILD)
+$(BUILD)%.o: $(SOURCE)%.s $(BUILD)
 	$(ARMGNU)-as $< -o $@
 
 # Rule to make the listing file.
--- src/kernel.s
+++ src/kernel.s
@@ -13,41 +13,16 @@
 @; Property tag message: 130 1 (ACT_LED pin number followed by state)
 @;
 
+.section .init
 .global _start @; Make _start available to the outside world
 
-.section .data
-.align 4 @; This ensures lowest 4 bits are 0 for the following label
-PropertyInfo:
-  @; = Message Header =
-  .int PropertyInfoEnd - PropertyInfo @; Calculate buffer size
-  .int 0 @; Request code: Process Request
-  @; = Tag Header =
-  .int 0x00038041 @; Tag ID (SET_GPIO_STATE)
-  .int 8 @; Value buffer size
-  .int 0 @; Request/response size
-  @; = Tag Value Buffer =
-  .int 130 @; ACT_LED pin number
-  .int 1 @; Turn it on
-  .int 0 @; End tag
-PropertyInfoEnd:
+_start:
+  mov sp, #0x8000 @; Set up the stack pointer
+  b main
 
 .section .text
-_start:
-  mailbox .req r0 @; Alias mailbox to r0
-  ldr r0, =0x3f00b880 @; Load the mailbox's base address into r0
+main:
+  b SetActLEDState
 
   wait1$:
-    status .req r1 @; Alias status to r1
-    ldr status, [mailbox, #0x18] @; Load the Mailbox 0 status address
-    tst status, #0x80000000 @; Check the status against the FULL bit
-    .unreq status @; Unset the alias
-    bne wait1$ @; Keep checking the mailbox until it isn't full
-
-  message .req r1 @; Alias message to r1
-  ldr message, =PropertyInfo @; Load r1 with address of our message buffer
-  add message, #8 @; Put the channel in the last 4 bits
-  str message, [mailbox, #0x20] @; Put the message in the mailbox
-  .unreq message @; Unset the alias
-
-  wait2$:
-    b wait2$ @; Give the CPU something to do ad infinitum
+    b wait1$ @; Give the CPU something to do ad infinitum
